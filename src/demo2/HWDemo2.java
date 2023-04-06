4e41ea747a6d558247a212b1d6db3ac88af8ff56
ian_c <ian_c@Thetool>
2023-04-05 23:15:18 -0400

    Initial Commit

---
+++ src/demo2/HWDemo2.java
@@ -0,0 +1,16 @@
+package demo2;
+
+public class HWDemo2 {
+	public static void main(String[] args) {
+		System.out.println("Test");
+		
+		
+		System.out.println("GitHub HW");
+		
+	}
+	
+	
+	
+	
+
+}
